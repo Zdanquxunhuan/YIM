21313a3f7b97296542207c21608b799e529bb522
ZCY <982287281@qq.com>
2023-09-20 22:36:13 +0800

    cim server

---
+++ yim-server/src/main/java/com/yu/yimserver/server/CIMServer.java
@@ -0,0 +1,14 @@
+package com.yu.yimserver.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class CIMServer {
+
+    public void start(){
+        ServerBootstrap bootstrap = new ServerBootstrap();
+    }
+}
